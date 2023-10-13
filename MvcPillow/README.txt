11c41a7a1a07ee52c268cb06d2befc651ea4245a
estacimo <w0825062@myscc.ca>
2023-10-13 13:57:02 -0400

    Registered Db context and added connection string

--- MvcPillow/README.txt
+++ MvcPillow/README.txt
@@ -24,3 +24,6 @@ Added the properties (Id, Size, Purpose, Density, Color, Shape, Material, Price)
 
 1324
 Updated the Data folder and the MvcPillowContext.cs file
+
+1354
+Made changes to Startup.ConfigureServices and added a connection string in the appsettings.json file
--- MvcPillow/Startup.cs
+++ MvcPillow/Startup.cs
@@ -28,11 +28,13 @@ namespace MvcPillow
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<MvcPillowContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("MvcPillowContext"))); //from DefaultConnection to MvcPillowContext
+            
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<MvcPillowContext>();
+           
             services.AddControllersWithViews();
+           
             services.AddRazorPages();
         }
 
